62edc8b9a4d97bc472c3ac42f70be14f238c2103
KienNB <kiennb_hn@asiasoft.com.vn>
2022-09-06 11:19:07 +0700

    .

---
+++ Common/Captcha.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class Captcha
+    {
+        private class ReCaptchaResponse
+        {
+            public bool Success;
+            public string ChallengeTs;
+            public string Hostname;
+            public object[] ErrorCodes;
+        }
+        public static bool ValidCaptcha(string token)
+        {
+            bool isHuman = true;
+
+            try
+            {
+                string secretKey = "6Lcx-vUfAAAAAJAVKkyybKlDHLSBlicWEyqUvK8K";
+                Uri uri = new Uri("https://www.google.com/recaptcha/api/siteverify" +
+                                  $"?secret={secretKey}&response={token}");
+                HttpWebRequest request = WebRequest.CreateHttp(uri);
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = 0;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                StreamReader streamReader = new StreamReader(responseStream);
+                string result = streamReader.ReadToEnd();
+                ReCaptchaResponse reCaptchaResponse = JsonConvert.DeserializeObject<ReCaptchaResponse>(result);
+                isHuman = reCaptchaResponse.Success;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return isHuman;
+        }
+    }
+}
---
+++ Common/Hex.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Hex
+    {
+
+        public static string Text2Hex(string hex)
+        {
+            byte[] ba = Encoding.Default.GetBytes(hex);
+            var hexString = BitConverter.ToString(ba);
+            hexString = hexString.Replace("-", " ");
+            return hexString;
+        }
+
+        public static byte[] Hext2Text(string hex)
+        {
+            hex = hex.Replace(" ", "");
+            byte[] raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return raw;
+        }
+    }
+}

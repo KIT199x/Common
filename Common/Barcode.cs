3d541dd0ede6dd52469d0c03025e94f470dc3fda
Nguyễn Bá Kiên <nguyenbakienofficial@hotmail.com>
2022-11-14 19:05:19 +0700

    Thêm Barcode

---
+++ Common/Barcode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using ZXing.QrCode;
+
+namespace Common
+{
+    public static class Barcode
+    {
+        public static string Translate(string _data)
+        {
+            var width = 450;
+            var height = 100;
+            var barcodeWriterPixelData = new ZXing.BarcodeWriterPixelData
+            {
+                Format = ZXing.BarcodeFormat.CODE_128,
+                Options = new QrCodeEncodingOptions
+                {
+                    Height = height,
+                    Width = width,
+                    PureBarcode = true,
+                    Margin = 0
+                }
+            };
+            var pixelData = barcodeWriterPixelData.Write(_data);
+            using (var bitmap = new Bitmap(pixelData.Width, pixelData.Height, PixelFormat.Format32bppRgb))
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    var bitmapData = bitmap.LockBits(new Rectangle(0, 0, pixelData.Width, pixelData.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppRgb);
+                    try
+                    {
+                        Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
+                    }
+                    finally
+                    {
+                        bitmap.UnlockBits(bitmapData);
+                    }
+                    bitmap.Save(memoryStream, ImageFormat.Png);
+                    return String.Format("data:image/png;base64,{0}", Convert.ToBase64String(memoryStream.ToArray()));
+                }
+            }
+        }
+    }
+}
---
+++ Common/QRcode.cs
@@ -0,0 +1,33 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class QRcode
+    {
+        public static string CreateQRCode(string QRText)
+        {
+            QRCodeGenerator _qrCode = new QRCodeGenerator();
+            QRCodeData _qrCodeData = _qrCode.CreateQrCode(QRText, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(_qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            var QrUri = BitmapToBytesCode(qrCodeImage);
+            return "data:image/png;base64," + Convert.ToBase64String(QrUri);
+        }
+        private static Byte[] BitmapToBytesCode(Bitmap image)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                image.Save(stream, ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+    }
+}
